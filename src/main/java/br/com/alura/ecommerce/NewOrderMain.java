ca1f3094287b8cad01678b7bccc8050946d557b5
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2020-10-29 13:13:20 -0300

    Usar chave aleatoria para balancear entre as particoes.

--- src/main/java/br/com/alura/ecommerce/NewOrderMain.java
+++ src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -7,6 +7,7 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
+import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 /*
@@ -17,14 +18,18 @@ C:\kafka_2.13-2.6.0\bin\windows>kafka-server-start.bat ..\..\config\server.prope
 
 PARA CONSUMIR AS MENSAGENS PRODUZIDAS POR ESSA CLASSE
 kafka-console-consumer.bat --bootstrap-server localhost:9092 --topic ECOMMERCE_NEW_ORDER --from-beginning
+
+CONFIGURAR 3 PARTICOES
+kafka-topics.bat --alter --zookeeper localhost:2181 --topic ECOMMERCE_NEW_ORDER --partitions 3
  */
 
 public class NewOrderMain {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         var producer = new KafkaProducer<String, String>(properties());
+        var key = UUID.randomUUID().toString();
         var value = "132123,67523,7894589745";
-        var record = new ProducerRecord<String, String>("ECOMMERCE_NEW_ORDER", value, value);
+        var record = new ProducerRecord<String, String>("ECOMMERCE_NEW_ORDER", key, value);
         Callback callback = (data, ex) -> {
             if (ex != null) {
                 ex.printStackTrace();
@@ -34,7 +39,7 @@ public class NewOrderMain {
                     + data.partition() + " / offset " + data.offset() + " / timestamp " + data.timestamp());
         };
         var email = "Thank you for your order! We are processing your order!";
-        var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", email, email);
+        var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", key, email);
         producer.send(record, callback).get();
         producer.send(emailRecord, callback).get();
     }
