251e2764ba4898cb4172e1e39563baf17890c761
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2020-10-30 10:35:06 -0300

    Refactory no producer.

--- src/main/java/br/com/alura/ecommerce/NewOrderMain.java
+++ src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -26,32 +26,15 @@ kafka-topics.bat --alter --zookeeper localhost:2181 --topic ECOMMERCE_NEW_ORDER 
 public class NewOrderMain {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        var producer = new KafkaProducer<String, String>(properties());
+        var dispatcher = new KafkaDispatcher();
 
-        for (var i=0; i < 20; i++) {
+        for (var i=0; i < 10; i++) {
             var key = UUID.randomUUID().toString(); //A chave é usada para distribuir a mensagem entre as partições existentes e consequentemente entre as instâncias de um serviço dentro de um consumer group.
             var value = "132123,67523,7894589745";
-            var record = new ProducerRecord<String, String>("ECOMMERCE_NEW_ORDER", key, value);
-            Callback callback = (data, ex) -> {
-                if (ex != null) {
-                    ex.printStackTrace();
-                    return;
-                }
-                System.out.println("sucesso enviando " + data.topic() + " ::: partition "
-                        + data.partition() + " / offset " + data.offset() + " / timestamp " + data.timestamp());
-            };
-            var email = "Thank you for your order! We are processing your order!";
-            var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", key, email);
-            producer.send(record, callback).get();
-            producer.send(emailRecord, callback).get();
-        }
-    }
+            dispatcher.send("ECOMMERCE_NEW_ORDER", key, value);
 
-    private static Properties properties() {
-        var properties = new Properties();
-        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        return properties;
+            var email = "Thank you for your order! We are processing your order!";
+            dispatcher.send("ECOMMERCE_SEND_EMAIL", key, email);
+        }
     }
 }
